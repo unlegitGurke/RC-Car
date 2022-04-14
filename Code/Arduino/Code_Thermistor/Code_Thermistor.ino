489c32d08ea9f53425c2ae7a12d66543534c75a0
unlegitGurke <70239056+unlegitGurke@users.noreply.github.com>
2022-04-15 00:57:24 +0200

    V5.37
    V5.37

---
+++ Code/Arduino/Code_Thermistor/Code_Thermistor.ino
@@ -0,0 +1,27 @@
+int tempPin = 0;
+
+void setup(){
+
+  Serial.begin(9600);
+
+}
+
+void loop(){
+  
+  int tempReading = analogRead(tempPin);
+  // This is OK
+  double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
+  tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );       //  Temp Kelvin
+  float tempC = tempK - 273.15;            // Convert Kelvin to Celcius
+  float tempF = (tempC * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
+  /*  replaced
+    float tempVolts = tempReading * 5.0 / 1024.0;
+    float tempC = (tempVolts - 0.5) * 10.0;
+    float tempF = tempC * 9.0 / 5.0 + 32.0;
+  */
+  // Display Temperature in C
+  Serial.print("Temp: ");
+  Serial.println(tempC);
+  delay(500);
+  
+}
