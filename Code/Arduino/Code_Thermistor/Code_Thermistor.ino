5203647ee3237fea1979b194bfc087b5e87c0755
unlegitGurke <70239056+unlegitGurke@users.noreply.github.com>
2022-04-16 02:12:54 +0200

    V5.39
    V5.39

--- Code/Arduino/Code_Thermistor/Code_Thermistor.ino
+++ Code/Arduino/Code_Thermistor/Code_Thermistor.ino
@@ -1,4 +1,8 @@
-int tempPin = 0;
+const int NrOfSensors = 6;    //Define Number of Sensors connected to the Analog Pins of the Arduino.
+
+const int TempUnit = 2;   //Choose which int which Unit the Temperature will be printed. 1 for Kelvin, 2 for Celcius, 3 for Fahrenheit.
+
+float temp[NrOfSensors + 1][3];
 
 void setup(){
 
@@ -7,21 +11,30 @@ void setup(){
 }
 
 void loop(){
+
+  for(int i = 0;i <= NrOfSensors - 1;i++){    //Read Temps from all sensors and prints them.
+    ReadTemp(i);
+    Serial.print("Temp");
+    Serial.print(i + 1);
+    Serial.print(": ");
+    Serial.println(temp[i][TempUnit]);
+  }
   
-  int tempReading = analogRead(tempPin);
-  // This is OK
+  Serial.println("");
+  delay(500);
+  
+}
+
+void ReadTemp(int Pin) {
+  
+  int tempReading = analogRead(Pin);
   double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
   tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );       //  Temp Kelvin
   float tempC = tempK - 273.15;            // Convert Kelvin to Celcius
   float tempF = (tempC * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
-  /*  replaced
-    float tempVolts = tempReading * 5.0 / 1024.0;
-    float tempC = (tempVolts - 0.5) * 10.0;
-    float tempF = tempC * 9.0 / 5.0 + 32.0;
-  */
-  // Display Temperature in C
-  Serial.print("Temp: ");
-  Serial.println(tempC);
-  delay(500);
+
+  temp[Pin][1] = tempK;
+  temp[Pin][2] = tempC;
+  temp[Pin][3] = tempF;
   
 }
