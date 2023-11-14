83d5b61f22282eadbcf46c8597c34e4c9d3a0b30
unlegitGurke <70239056+unlegitGurke@users.noreply.github.com>
2023-11-14 23:32:08 +0100

    V6.79
    V6.79

---
+++ Code/Arduino/UltrasonicSensor_Test/UltrasonicSensor_Test.ino
@@ -0,0 +1,24 @@
+const int trigPin = 18;
+const int echoPin = 19;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+  long distance = duration * 0.034 / 2;
+
+  Serial.print("Distance:");
+  Serial.println(distance);
+  delay(100);
+}
